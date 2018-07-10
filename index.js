f580b7e3bac553a809a3f7d4e72f28040f81093b
ShiroanXyz <40276172+ShiroanXyz@users.noreply.github.com>
2018-07-10 10:08:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ client.on('guildMemberAdd', (member) => {
     member.sendMessage({
         embed: {
             color: 0xf44242,
-            description: 'Hey I created a server on Anime & Gaming Join us!\n**Here\'s the invite: __https://discord.gg/EbxWzQ3__**'
+            description: 'Do you want To join my server? ♡\n**Here\'s the invite: __https://discord.gg/EbxWzQ3__**',
+            image; {
+             url: 'https://cdn.discordapp.com/attachments/464714834974474240/466153658363412491/hug-S1a0DJhqG.gif'
+          }
         }
     });
 });
