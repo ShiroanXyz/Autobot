f00488270ffef092c84e31003f2cb83d1f9a9545
ShiroanXyz <40276172+ShiroanXyz@users.noreply.github.com>
2018-07-10 10:15:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ client.on('guildMemberAdd', (member) => {
     member.sendMessage({
         embed: {
             color: 0xf44242,
-            description: 'Do you want To join my server? ♡\n**Here\'s the invite: __https://discord.gg/EbxWzQ3__**',
+            description: 'Do you want to join my server? ♡\n**Here\'s the invite: __https://discord.gg/EbxWzQ3__**',
             image: {
              url: 'https://cdn.discordapp.com/attachments/464714834974474240/466153658363412491/hug-S1a0DJhqG.gif'
           }
